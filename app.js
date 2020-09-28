d8544155133f1986d8e36c2f17df982c234c4793
Varun Tomar <vrnkmr119@gmail.com>
2020-09-28 17:43:11 +0530

    intial commit

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-// Tutorial by http://youtube.com/CodeExplained
-// api key : 82005d27a116c2880c8f0fcb866998a0
 
+//Weather App 
 
 const iconElement = document.querySelector(".weather-icon");
 const tempElement = document.querySelector(".temperature-value p");
